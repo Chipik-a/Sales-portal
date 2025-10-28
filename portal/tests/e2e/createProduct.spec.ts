fdc8893ed7807fd96eae40894c03adbe0fe8b14f
Anna Popelniukh <13nuta@gmail.com>
2025-10-28 17:22:51 +0200

    Add portal folder as regular files

---
+++ portal/tests/e2e/createProduct.spec.ts
@@ -0,0 +1,41 @@
+import { test, expect } from "@playwright/test";
+import { SignInPage } from "../../src/pages/signIn.page";
+import { HomePage } from "../../src/pages/homePage.page";
+import { ProductsPage } from "../../src/pages/product.page";
+import { credentials } from "../../config/env";
+import { generateProductData } from "../../src/data/salesPortal/products/generateProductData";
+
+test.describe("[Sales Portal] [Add Products]", () => {
+  test("Add new product and verify", async ({ page }) => {
+    const signInPage = new SignInPage(page);
+    const homePage = new HomePage(page);
+    const productsPage = new ProductsPage(page);
+
+    await signInPage.open();
+    await expect(signInPage.emailInput).toBeVisible();
+
+    await signInPage.fillCredentials(credentials.username, credentials.password);
+    await signInPage.clickLogin();
+
+    await signInPage.waitForLoaded();
+    await expect(homePage.welcomeText).toBeVisible();
+
+    await homePage.clickOnViewModule("Products");
+
+    await productsPage.uniqueElement.waitFor({ state: "visible", timeout: 60000 });
+
+    const product = generateProductData();
+    await productsPage.addProduct(
+        product.name,
+        product.manufacturer,
+        product.price.toString(),
+        product.amount.toString()
+    );
+
+    await expect(productsPage.toastMessage).toHaveText("Product was successfully created");
+
+    await productsPage.verifyFirstRow(product.name, product.price.toString());
+  });
+});
+
+//add
